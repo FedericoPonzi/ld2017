c7155dbda48aa45e3cb91d2535c5f40babd1b2dd
FedericoPonzi <isaacisback92@gmail.com>
2017-05-24 10:29:49 +0200

    Adds position

--- about-us.php
+++ about-us.php
@@ -60,18 +60,68 @@ Contatti</a>
               <p>Il <strong>LUG di Roma Tre</strong> è sempre stato, negli anni passati, conosciuto al livello italiano per la bravura nell'utilizzo ottimizzato di Linux e per la passione che mettevano nello spiegare e nell'insegnare la filosofia Linux agli altri. Elementi del vecchio LUG hanno peraltro tenuto conferenze al livello nazionale riguardo sviluppo e debug degli aggiornamenti, e noi puntiamo a non essere meno.</p>
               <p>Nel nostro LUG noi sviluppiamo tutto ciò che serve al nostro SO preferito, forniamo supporto e assistenza a chi è nuovo nel mondo di Linux e cerchiamo, attraverso supporto e corsi di far comprendere alle persone le potenzialità pressochè infinite di questo SO.</p>
               <p>Vi terremo informati su futuri eventi che faremo, e per i veri appassionati ricordatevi che qualsiasi informazione aggiuntiva, domanda o chiarimento basta contattarci sulla nostra pagina Facebook: <a href="https://www.facebook.com/LUGRoma3">LUG Roma Tre</a>!</p>
-            </div><!-- / Fine main -->
+            <h2 class="title">Dove siamo:</h2 >
+            <p>Università degli Studi Roma Tre, Dipartimento di Ingegneria. Via Vito Volterra, 48, Roma, Italia.</p>
+            <div class="google-maps">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11886.894909781871!2d12.470625!3d41.855773!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13258a91896c229f%3A0xd9f902e1b49a03ba!2sVia+Vito+Volterra%2C+48%2C+00146+Roma%2C+Italia!5e0!3m2!1sit!2sit!4v1495614107067" width="600" height="450" frameborder="0" style="border:0"></iframe>
+            </div>
+            <h3>Con mezzo privato</h3>
+            <p> Clicca sulla mappa da mobile e imposta il navigatore!</p>
+            <p> È possibile a studenti e visitatori fruire dell’ampio parcheggio interno della Facoltà, cui si accede da Via
+            Umberto Nistri 9.
+            </p>
+            <h3>Dal G.R.A.</h3>
+            <p> Volendo raggiungere Roma con mezzo proprio, la via più diretta è (anche se si arriva da Nord o Est: è sempre
+            consigliabile evitare il transito per il Centro), dal GRANDE RACCORDO ANULARE, percorrere dall'uscita 26 (Roma
+            centro/Eur) Via Cristoforo Colombo, dopo un chilometro svoltare a sinistra allo svincolo per Viale Guglielmo
+            Marconi, dopo un chilometro svoltare a sinistra a Via Corrado Segre.</p>
+            <h2>Con mezzi pubblici</h2>
+            <p> Attorno alla zona esistono anche linee ATAC, con fermate vicine ai nostri edifici:
+            </p>
+            <ul>
+            <li>170 o 791 alla fermata "Marconi - Edison"</li>
+            </ul>
+            <p> Dare le spalle al ponte e girare in Via Vito Volterra (la prima a destra), percorrerla tenendo la destra fino a
+            che non si raggiunge l'entrata per Roma Tre.
+            </p>
+            <ul>
+              <li>170, 791 o 670 alla fermata "Marconi - Bortolotti"</li>
+            </ul>
+            <p> Andate verso il semaforo, attraversate Via Guglielmo Marconi, proseguire dritto verso Via Corrado Segre tenendo
+            la destra, dopo 200 metri circa troverete l'entrata principale di Roma Tre
+            </p>
+            <p> In alternativa, la sede della Facoltà è prossima (meno di dieci minuti a piedi) alla fermata SAN PAOLO della
+            Metropolitana linea B REBIBBIA-LAURENTINA.
+            </p>
+            <p> Attraversare Via Ostiense e raggiungere Viale Guglielmo Marconi, attraversarla e fare riferimento alle
+            istruzioni riportate sopra in riferimento alla fermata "Marconi - Edison".
+            </p>
+            <h2>Con il treno</h2>
+            <p> Se si opta per il treno, si consideri che le principali stazioni ferroviarie della città (TERMINI, TIBURTINA,
+            OSTIENSE) sono servite dalla linea B della Metropolitana.
+            </p>
+            <h2>Dall'aereoporto</h2>
+            <p> Dall’aeroporto Leonardo da Vinci di Fiumicino, se automuniti percorrere l’autostrada ROMA-FIUMICINO fino
+            all’intersezione con il GRA, poi fare riferimento alle istruzioni riportate sopra.
+            </p>
+            <p> Se si opta per il mezzo pubblico, delle due possibilità alla stazione ferroviaria dell’aeroporto è preferibile
+            il treno AEROPORTO-FARA SABINA: scesi alla stazione ROMA OSTIENSE, raggiungere (meno di due minuti a piedi per
+            camminamenti interni) la fermata PIRAMIDE della Metropolitana linea B, prenderla nella direzione LAURENTINA e
+            scendere alla fermata BASILICA S. PAOLO.
+            </p>
+            <p> Dall’aeroporto G.B.Pasine di Ciampino, se automuniti percorrere la VIA APPIA NUOVA (SS7) in direzione Centro
+            fino all’intersezione con il GRA, poi fare riferimento alle istruzioni riportate sopra; se si opta per il mezzo
+            pubblico, la via più agevole è il pullman COTRAL che conduce alla fermata ANAGNINA della Metropolitana linea A;
+            raggiunta la fermata TERMINI, cambiare linea per la B, prenderla nella direzione LAURENTINA e scendere alla
+            fermata BASILICA S. PAOLO, poi fare riferimento alle istruzioni riportate sopra.
+            </p>
+            </article>
 
-            <?php include_once "partners.php" ?>
-      </div>
-    </div><!-- /.container -->
+    </div><!-- / Fine main -->
 
-    <div class="container main">
-        <h3>Dove siamo:</h3>
-        <p>Via casa mia</p>
-        <h4>Indicazioni</h4>
-        <p>Dal raccordo: </p>
-    </div>
+    <?php include_once "partners.php" ?>
+  </div>
+  </div><!-- /.container -->
     <?php include_once "footer.php" ?>
   </body>
 </html>
