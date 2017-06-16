593b1d72b9198402a7cc1d9311f1fb6b1d9a1e93
FedericoPonzi <isaacisback92@gmail.com>
2017-06-16 16:00:56 +0200

    rimuove offset inutile

--- partners.php
+++ partners.php
@@ -1,4 +1,4 @@
-<aside class="col-md-offset-1 col-md-3 col-sm-offset-0 col-sm-4 col-xs-offset-0 col-xs-4">
+<aside class="col-md-4 col-sm-offset-0 col-sm-4 col-xs-offset-0 col-xs-4">
   <h4>Partners:</h4>
   <div class="row">
     <div class="col">
