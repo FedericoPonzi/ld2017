877d77aefde4787f0ec24041128af07680664abe
iksoiks <christian.cricri94@gmail.com>
2017-06-27 11:34:31 +0200

    Fixed c4p

--- c4p.php
+++ c4p.php
@@ -42,17 +42,17 @@
 
 <p>Il Linux Day è l’evento nazionale dedicato a tutti coloro che sposano la filosofia di GNU/Linux, del Software Libero ed Open Source ed, in generale, a tutti i curiosi di questo mondo.</p>
 
-<p>Collabora anche tu mettendo a disposizione professionalità, entusiasmo e passione! Se hai in mente un argomento interessante e vuoi collaborare all’evento, inviaci la tua proposta per uno speech od un workshop!</p>
+<p>Collabora anche tu mettendo a disposizione professionalità, entusiasmo e passione! Se hai in mente un argomento interessante e vuoi collaborare all’evento, inviaci la tua proposta per uno speech od un workshop!</p>s
 
 <p>Ricorda però che l’evento è dedicato a GNU/Linux, daremo quindi precedenza ai talk dedicati ad esso, considerando in seguito tutto quel che comunque riguarda l’ambito Free and Open Source Software/Hardware. Proposte non inerenti non verranno prese in considerazione.</p>
 
 <p>Quest'anno il tema affrontato durante il Linux Day sarà il diritto alla privacy ed alla riservatezza individuale.</p>
 
-<p>Come Partecipare: per proporre il tuo intervento basta inviare (entro e non oltre il 3 Settembre) una e-mail a roma2lug[at]gmail[dot]com con riferimento in oggetto <strong>[CFP17]</strong>.</p>
+<p>Come Partecipare: per proporre il tuo intervento basta inviare (entro e non oltre il 3 Settembre) una e-mail a <strong>roma2lug[at]gmail[dot]com</strong> con riferimento in oggetto <strong>[CFP17]</strong>.</p>
 
 <p>Indica nome e cognome del/i relatore/i, e-mail, numero di telefono, una breve biografia personale, il titolo dell’intervento e un abstract riguardante il contenuto che proponi!</p>
 
-<p>Per informazioni puoi continuare a seguire il sito ld17.lugroma3.it oppure contattarci alle mail <a href="mailto:info@lugroma3.it">info@lugroma3.it</a> e <a href="mailto:roma2lug@gmail.com">roma2lug@gmail.com</a> con riferimento in oggetto <strong>[LD17]</strong>!</p>
+<p>Per informazioni puoi continuare a seguirci su questo sito oppure contattarci tramite il <a href="/contatti.php">form di contatto</a>.</p>
 
           </div>
           <?php include_once "partners.php" ?>
