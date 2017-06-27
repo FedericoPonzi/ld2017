667edf0542ea126cb9f326c6e09fdb8499d2e3e0
FedericoPonzi <isaacisback92@gmail.com>
2017-06-27 12:50:58 +0200

    Nasconde pulsante donazioni

--- partners.php
+++ partners.php
@@ -22,13 +22,14 @@
     </div>
   </div>
   <div class="row">
-  <div style="padding-top:30px; margin: 0 auto;">
-    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-    <input type="hidden" name="cmd" value="_s-xclick">
-    <input type="hidden" name="hosted_button_id" value="B5QSQDEMKHWX8">
-    <input type="image" src="https://www.paypalobjects.com/it_IT/IT/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal è il metodo rapido e sicuro per pagare e farsi pagare online.">
-    <img alt="" border="0" src="https://www.paypalobjects.com/it_IT/i/scr/pixel.gif" width="1" height="1">
-    </form>
-  </div>
+    <!-- display:none dovrai stare, finchè un iban non compare -->
+    <div style="display:none; padding-top:30px; margin: 0 auto;">
+      <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+      <input type="hidden" name="cmd" value="_s-xclick">
+      <input type="hidden" name="hosted_button_id" value="B5QSQDEMKHWX8">
+      <input type="image" src="https://www.paypalobjects.com/it_IT/IT/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal è il metodo rapido e sicuro per pagare e farsi pagare online.">
+      <img alt="" border="0" src="https://www.paypalobjects.com/it_IT/i/scr/pixel.gif" width="1" height="1">
+      </form>
+    </div>
   </div>
 </aside>
