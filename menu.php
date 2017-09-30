6946bbcfc5b10f0c4f5b4a93deeea8df6017a382
Federico Ponzi <FedericoPonzi@users.noreply.github.com>
2017-09-30 23:11:32 +0200

    Update menu.php

--- menu.php
+++ menu.php
@@ -16,7 +16,7 @@ $menu_items = array(new MenuItem("Home", "index.php", "home"),
                    new MenuItem("C4P", "c4p.php", "envelope-open"),
 
                     new MenuItem("Agenda", "agenda.php", "calendar"),
-                   new MenuItem("Biglietti", "https://www.eventbrite.it/e/biglietti-linux-day-2017-roma-35650874726", "ticket"),
+                   new MenuItem("Biglietti", "https://www.eventbrite.it/e/biglietti-corso-base-linux-38426264993", "ticket"),
                    new MenuItem("Sponsors", "sponsors.php", "star"),
                    new MenuItem("About us", "about-us.php", "info-circle"),
                    new MenuItem("Contatti", "contatti.php", "envelope-o"),
